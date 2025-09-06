f157b795d7dc953b2b3284ccf73c536e000b7e16
OnlyJustEthan <148938693+OnlyJustEthan@users.noreply.github.com>
2025-09-05 20:02:15 -0400

    Readme Algo + Working printValues and swap

---
+++ bAndPMain.c
@@ -0,0 +1,78 @@
+/* Alogrithm
+ * constant MAX is max length of array
+ * 
+ * function printValues (values):
+ * 	int s = 0
+ * 	print("[")
+ * 	while(s > MAX):
+ * 		print(item)
+ * 	print("]\n")
+ *
+ * function sort (array):			//Provided function algo don't touch
+ * 	create integer variables i and j
+ * 	for i from zero to MAX - 1:
+ * 		for j from zero to MAX - 1:
+ * 			if array[j] > array[j+1]:
+ * 				swap array[j] with array[j+1]
+ * 				printArray(array)
+ *
+ * function swap (current value address, next value address):
+ * 	create temporary value holder variable
+ * 	copy value at current address to temp holder
+ * 	copy next value to current value
+ * 	copy temp value to next value
+ */
+
+#include <stdio.h> 				//Starter don't touch
+const int MAX=9; 				//Starter don't touch
+
+void printValues(int*); 			//Starter don't touch
+void sort(int*);				//Starter don't touch
+void swap(int*, int*); 				//Starter don't touch
+
+int main(){ 					//Starter don't touch
+  int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5}; 	//Starter don't touch
+  printf("Before: \n"); 			//Starter don't touch
+  printValues(values); 				//Starter don't touch
+
+  // test swap 					//Starter don't touch
+  int x = 3; 					//Starter don't touch
+  int y = 5; 					//Starter don't touch
+  printf("x: %d, y: %d \n", x, y); 		//Starter don't touch
+  swap(&x, &y); 				//Starter don't touch
+  printf("x: %d, y: %d \n", x, y);		//Starter don't touch
+
+  //sort(values);
+  printf("After: \n"); 				//Starter don't touch
+  printValues(values); 				//Starter don't touch
+
+  return(0); 					//Starter don't touch
+} // end main 					//Starter don't touch
+
+void printValues(int array[]){
+	int s = 0;
+	//printf("Const understanding sanity check: %d\n", MAX);
+	printf("[");
+	while(s < MAX){
+		s++;
+		printf("%d ",array[s]);
+	} // end while
+	printf("]\n");
+} // end printValues
+
+/*
+void sort(int values[]){
+	int i = 0;
+	int j = 0;
+} // end sort
+*/
+
+void swap(int* c, int* n){
+	int t = 0;
+	printf("Pre swap values: %d, %d, %d\n", t, *c, *n);
+	t = *c; // t becomes the value at current
+	*c = *n; // value at next becomes value at current 
+	*n = t; // value at next becomes t (The old C)
+	printf("Post swap values: %d, %d, %d\n", t, *c, *n);
+}
+	
